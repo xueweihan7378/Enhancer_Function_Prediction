a0732c01e642add16d07ab763643b081ed56585e
Caitlin <caitlimm@users.noreply.github.com>
2020-12-15 16:49:01 -0800

    Add files via upload

---
+++ features_pipeline/eqtl_Zstats.R
@@ -0,0 +1,10 @@
+links <- read.delim("C:/Users/caitlinAdmin/Downloads/Pipeline/pre_eQTL_Breast_links", header=FALSE)
+#View(links)
+colnames(links)<-c('enhancer', 'gene', 'tissue', 'k', 'chistat')
+head(links,5)
+links$pchisq<-pchisq(links$chistat, df=(2*links$k), lower.tail=FALSE)
+links$Zstat<-(sqrt(links$chistat/(2*links$k)))
+#This transforms the chi-squared statistic into a Z statistic for ease of interpretability
+#because the chi-squared df differ among the links.
+write.table(links, file="Breast_eQTL_Zstats.txt", append = FALSE, quote=FALSE, sep = "\t", dec = ".",
+            row.names = FALSE, col.names = TRUE)
